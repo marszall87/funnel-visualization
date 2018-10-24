3fc3e0767cd735fb3800a2e6b6940e94c28e37de
Michał Nykiel <michal.nykiel@spartez.com>
2018-10-24 15:59:31 +0200

    add popups for flows and drops

--- src/index.js
+++ src/index.js
@@ -22,13 +22,13 @@ class Popup extends Component {
     }
 }
 
-class Bucket extends Component {
+class WithPopup extends Component {
     constructor() {
         super();
         this.state = {
             showPopup: false
         };
-        this.bucketElement = undefined;
+        this.triggerElement = undefined;
     }
 
     togglePopup() {
@@ -36,15 +36,17 @@ class Bucket extends Component {
     }
 
     componentDidMount() {
-        this.bucketElement;
+        this.triggerElement;
     }
+}
 
+class Bucket extends WithPopup {
     render({ bucket, opts }) {
         const { id, color, x, y, title, value, height, clients } = bucket;
         const { bucketWidth, borderRadius } = opts;
         return (
             <g
-                ref={el => (this.bucketElement = el)}
+                ref={el => (this.triggerElement = el)}
                 class={style.bucket}
                 transform={`translate(${x} ${y})`}
                 onClick={() => this.togglePopup()}
@@ -73,9 +75,70 @@ class Bucket extends Component {
                         {value}
                     </text>
                 </g>
-                {clients && clients.length > 0 ? (
-                    <Popup open={this.state.showPopup} triggerRef={this.bucketElement}>
-                        <h4>{title} clients</h4>
+                <Popup open={this.state.showPopup} triggerRef={this.triggerElement}>
+                    <h4>{title}</h4>
+                    {clients.map(client => (
+                        <p>
+                            <span class={`${style.label} ${style.label}-${client.label}`}>{client.label}</span>{' '}
+                            <a href={client.link}>{client.name}</a>
+                        </p>
+                    ))}
+                </Popup>
+            </g>
+        );
+    }
+}
+
+class Flow extends WithPopup {
+    render({ flow, opts }) {
+        const { from, to, height, fromOffset, toOffset, id, percent, clients, title } = flow;
+        const { bucketWidth, height: graphHeight } = opts;
+
+        const x1 = from.x + bucketWidth;
+        const x2 = to.x;
+        const y1 = from.y + (fromOffset || 0) + height / 2;
+        const y2 = to.y + (toOffset || 0) + height / 2;
+
+        const width = Math.abs(x1 - x2);
+        // const radius = 5 * (Math.abs(y1 - y2) / graphHeight) + 2;
+        const radius = bucketWidth / 3;
+        const straight = y1 === y2;
+        const extension = width > 2 * bucketWidth;
+        const curveX = extension ? width - bucketWidth : 0;
+
+        const gradientUrl = `url(#funnel-gradient-${id})`;
+
+        const fontSize = height < 22 ? Math.max(height, 12) : 22;
+        const labelRadius = 5;
+
+        if (straight) {
+            // draw a rectangle, because gradients don't work on straight lines o_O
+            return (
+                <g class={style.flow} transform={`translate(${x1} ${y1 - height / 2})`}>
+                    <rect x={0} y={0} width={width} height={height} fill={gradientUrl} />
+                    <rect x={0} y={0} width={width} height={height} fill="url(#flow-pattern)" />
+                    <g class={style['popup-trigger']} onClick={() => this.togglePopup()}>
+                        <rect
+                            x={width / 2 - fontSize - labelRadius}
+                            y={height / 2 - fontSize / 2 - labelRadius}
+                            width={fontSize * 2 + labelRadius * 2}
+                            height={fontSize + labelRadius * 2}
+                            rx={labelRadius}
+                            ry={labelRadius}
+                            fill={gradientUrl}
+                            ref={el => (this.triggerElement = el)}
+                        />
+                        <text
+                            x={width / 2}
+                            y={height / 2 + 1}
+                            alignment-baseline="middle"
+                            text-anchor="middle"
+                            font-size={`${fontSize / 22}em`}
+                        >
+                            {Math.floor(percent * 100)}%
+                        </text>
+                    </g>
+                    <Popup open={this.state.showPopup} triggerRef={this.triggerElement}>
                         {clients.map(client => (
                             <p>
                                 <span class={`${style.label} ${style.label}-${client.label}`}>{client.label}</span>{' '}
@@ -83,119 +146,106 @@ class Bucket extends Component {
                             </p>
                         ))}
                     </Popup>
-                ) : null}
+                </g>
+            );
+        } else {
+            const p1 = curveX + radius;
+            const p2 = width - radius;
+            const down = y1 < y2;
+            const path = [
+                `M ${0} ${y1}`,
+                extension ? `L ${curveX} ${y1}` : '',
+                `C ${down ? p1 : p2} ${y1}, ${down ? p2 : p1} ${y2}, ${width} ${y2}`
+            ].join(' ');
+
+            const textY = extension ? y1 : Math.min(y1, y2) + Math.abs(y1 - y2) / 2;
+
+            return (
+                <g class={style.flow} transform={`translate(${x1} 0)`}>
+                    <path d={path} fill="none" stroke={gradientUrl} stroke-width={height} />
+                    <path d={path} fill="none" stroke="url(#flow-pattern)" stroke-width={height} />
+                    <g class={style['popup-trigger']} onClick={() => this.togglePopup()}>
+                        <rect
+                            x={width / 2 - fontSize - labelRadius}
+                            y={textY - fontSize / 2 - labelRadius}
+                            width={fontSize * 2 + labelRadius * 2}
+                            height={fontSize + labelRadius * 2}
+                            rx={labelRadius}
+                            ry={labelRadius}
+                            fill={gradientUrl}
+                            ref={el => (this.triggerElement = el)}
+                        />
+                        <text
+                            x={width / 2}
+                            y={1}
+                            transform={`translate(0 ${textY})`}
+                            alignment-baseline="middle"
+                            text-anchor="middle"
+                            font-size={`${fontSize / 22}em`}
+                        >
+                            {Math.floor(percent * 100)}%
+                        </text>
+                    </g>
+                    <Popup open={this.state.showPopup} triggerRef={this.triggerElement}>
+                        {clients.map(client => (
+                            <p>
+                                <span class={`${style.label} ${style.label}-${client.label}`}>{client.label}</span>{' '}
+                                <a href={client.link}>{client.name}</a>
+                            </p>
+                        ))}
+                    </Popup>
+                </g>
+            );
+        }
+    }
+}
+
+class Drop extends WithPopup {
+    render({ drop, opts }) {
+        const { from, height, title, percent, clients } = drop;
+        const { bucketWidth } = opts;
+
+        const radius = 15;
+        const padding = 10;
+
+        const x = from.x + bucketWidth;
+        const y = from.y + from.height - height + radius;
+
+        const path = [`M 0 0`, `Q ${radius} 0, ${radius} ${radius}`].join(' ');
+
+        return (
+            <g transform={`translate(${x} ${y})`} class={style.drop}>
+                <path fill="none" d={path} stroke-width={radius * 2} />
+                <rect class={style.solid} x={0} y={radius} width={radius * 2} height={Math.max(height - radius, 0)} />
+                <rect x={0} y={height} width={radius * 2} height={padding} fill="url(#funnel-gradient-drop)" />
+                <rect
+                    x={0}
+                    y={-radius}
+                    width={radius * 2}
+                    height={height + padding + radius}
+                    fill="url(#drop-pattern)"
+                />
+                <g
+                    class={style['popup-trigger']}
+                    onClick={() => this.togglePopup()}
+                    ref={el => (this.triggerElement = el)}
+                >
+                    <text x={3} y={radius}>
+                        {Math.floor(percent * 100)}%
+                    </text>
+                </g>
+                <Popup open={this.state.showPopup} triggerRef={this.triggerElement}>
+                    {clients.map(client => (
+                        <p>
+                            <span class={`${style.label} ${style.label}-${client.label}`}>{client.label}</span>{' '}
+                            <a href={client.link}>{client.name}</a>
+                        </p>
+                    ))}
+                </Popup>
             </g>
         );
     }
 }
-
-const Flow = ({ flow, opts }) => {
-    const { from, to, height, fromOffset, toOffset, id, percent } = flow;
-    const { bucketWidth, height: graphHeight } = opts;
-
-    const x1 = from.x + bucketWidth;
-    const x2 = to.x;
-    const y1 = from.y + (fromOffset || 0) + height / 2;
-    const y2 = to.y + (toOffset || 0) + height / 2;
-
-    const width = Math.abs(x1 - x2);
-    const radius = 5 * (Math.abs(y1 - y2) / graphHeight) + 2;
-    const straight = y1 === y2;
-    const extension = width > 2 * bucketWidth;
-    const curveX = extension ? width - bucketWidth : 0;
-
-    const gradientUrl = `url(#funnel-gradient-${id})`;
-
-    const fontSize = height < 22 ? Math.max(height, 12) : 22;
-    const labelRadius = 5;
-
-    if (straight) {
-        // draw a rectangle, because gradients don't work on straight lines o_O
-        return (
-            <g class={style.flow} transform={`translate(${x1} ${y1 - height / 2})`}>
-                <rect x={0} y={0} width={width} height={height} fill={gradientUrl} />
-                <rect x={0} y={0} width={width} height={height} fill="url(#flow-pattern)" />
-                <rect
-                    x={width / 2 - fontSize - labelRadius}
-                    y={height / 2 - fontSize / 2 - labelRadius}
-                    width={fontSize * 2 + labelRadius * 2}
-                    height={fontSize + labelRadius * 2}
-                    rx={labelRadius}
-                    ry={labelRadius}
-                    fill={gradientUrl}
-                />
-                <text
-                    x={width / 2}
-                    y={height / 2 + 1}
-                    alignment-baseline="middle"
-                    text-anchor="middle"
-                    font-size={`${fontSize / 22}em`}
-                >
-                    {Math.floor(percent * 100)}%
-                </text>
-            </g>
-        );
-    } else {
-        const path = [
-            `M ${0} ${y1}`,
-            extension ? `L ${curveX} ${y1}` : '',
-            `C ${curveX + width / radius} ${y1}, ${width - width / radius} ${y2}, ${width} ${y2}`
-        ].join(' ');
-
-        const textY = extension ? y1 : Math.min(y1, y2) + Math.abs(y1 - y2) / 2;
-
-        return (
-            <g class={style.flow} transform={`translate(${x1} 0)`}>
-                <path d={path} fill="none" stroke={gradientUrl} stroke-width={height} />
-                <path d={path} fill="none" stroke="url(#flow-pattern)" stroke-width={height} />
-                <rect
-                    x={width / 2 - fontSize - labelRadius}
-                    y={textY - fontSize / 2 - labelRadius}
-                    width={fontSize * 2 + labelRadius * 2}
-                    height={fontSize + labelRadius * 2}
-                    rx={labelRadius}
-                    ry={labelRadius}
-                    fill={gradientUrl}
-                />
-                <text
-                    x={width / 2}
-                    y={1}
-                    transform={`translate(0 ${textY})`}
-                    alignment-baseline="middle"
-                    text-anchor="middle"
-                    font-size={`${fontSize / 22}em`}
-                >
-                    {Math.floor(percent * 100)}%
-                </text>
-            </g>
-        );
-    }
-};
-
-const Drop = ({ drop, opts }) => {
-    const { from, height, title, percent } = drop;
-    const { bucketWidth } = opts;
-
-    const radius = 15;
-    const padding = 10;
-
-    const x = from.x + bucketWidth;
-    const y = from.y + from.height - height + radius;
-
-    const path = [`M 0 0`, `Q ${radius} 0, ${radius} ${radius}`].join(' ');
-
-    return (
-        <g transform={`translate(${x} ${y})`} class={style.drop}>
-            <path fill="none" d={path} stroke-width={radius * 2} />
-            <rect class={style.solid} x={0} y={radius} width={radius * 2} height={Math.max(height - radius, 0)} />
-            <rect x={0} y={height} width={radius * 2} height={padding} fill="url(#funnel-gradient-drop)" />
-            <rect x={0} y={-radius} width={radius * 2} height={height + padding + radius} fill="url(#drop-pattern)" />
-            <text x={3} y={radius}>
-                {Math.floor(percent * 100)}%
-            </text>
-        </g>
-    );
-};
 
 const Step = ({ step, opts }) => {
     const { x, title } = step;
@@ -229,7 +279,8 @@ const layoutBuckets = (funnel, opts) => {
     return funnel.reduce((acc, step, stepIdx) => {
         const bucketCount = step.buckets.length;
         const stepBuckets = step.buckets.reduce((acc, bucket, bucketIdx) => {
-            if (bucket.value === 0) return acc;
+            const value = bucket.clients && bucket.clients.length;
+            if (value === 0) return acc;
             const x = 2 * stepIdx * bucketWidth;
             const y = acc.reduce((sum, b) => sum + b.height + bucketMargin, 0);
             return [
@@ -238,7 +289,8 @@ const layoutBuckets = (funnel, opts) => {
                     ...bucket,
                     x,
                     y,
-                    height: bucket.value * yScale
+                    height: value * yScale,
+                    value
                 }
             ];
         }, []);
@@ -257,9 +309,11 @@ const layoutFlows = (funnel, buckets, opts) => {
                 return [
                     ...acc,
                     ...bucket.flows.reduce((acc, flow) => {
-                        if (flow.value === 0) return acc;
+                        const value = flow.clients && flow.clients.length;
+                        if (value === 0) return acc;
                         const from = buckets.find(b => b.id === flow.source);
-                        const height = flow.value * yScale;
+                        const fromValue = from.clients && from.clients.length;
+                        const height = value * yScale;
                         const fromOffset = from.fromOffset || 0;
                         const toOffset = to.toOffset || 0;
                         from.fromOffset = fromOffset + height;
@@ -267,14 +321,16 @@ const layoutFlows = (funnel, buckets, opts) => {
                         return [
                             ...acc,
                             {
+                                ...flow,
                                 id: `${from.id}-${to.id}`,
                                 from,
                                 to,
-                                value: flow.value,
-                                percent: flow.value / from.value,
+                                value: value,
+                                percent: value / fromValue,
                                 height,
                                 fromOffset,
-                                toOffset
+                                toOffset,
+                                value
                             }
                         ];
                     }, [])
@@ -289,15 +345,20 @@ const layoutDrops = (funnel, buckets, opts) => {
 
     return funnel.reduce((acc, step) => {
         const stepDrops = step.buckets.reduce((acc, bucket) => {
-            if (!bucket.drop || bucket.drop.value === 0) return acc;
+            if (!bucket.drop) return acc;
             const from = buckets.find(b => b.id === bucket.id);
+            const value = bucket.drop.clients && bucket.drop.clients.length;
+            if (!value) return acc;
+            const bucketValue = bucket.clients && bucket.clients.length;
             return [
                 ...acc,
                 {
+                    ...bucket.drop,
                     from,
-                    height: bucket.drop.value * yScale,
+                    value,
+                    height: value * yScale,
                     title: bucket.drop.title,
-                    percent: bucket.drop.value / bucket.value
+                    percent: value / bucketValue
                 }
             ];
         }, []);
@@ -346,7 +407,7 @@ class Funnel extends Component {
         const height = this.state.height - headerHeight;
 
         const maxStepValueSum = funnel.reduce((max, step) => {
-            const stepValueSum = step.buckets.reduce((sum, b) => sum + (b.value || 0), 0);
+            const stepValueSum = step.buckets.reduce((sum, b) => sum + ((b.clients && b.clients.length) || 0), 0);
             return stepValueSum > max ? stepValueSum : max;
         }, 0);
 
@@ -445,13 +506,13 @@ class Funnel extends Component {
                             ))}
                         </g>
                         <g transform={`translate(0, ${headerHeight})`}>
-                            {drops.map(d => (
-                                <Drop drop={d} opts={opts} />
+                            {flows.map(f => (
+                                <Flow key={f.id} flow={f} opts={opts} />
                             ))}
                         </g>
                         <g transform={`translate(0, ${headerHeight})`}>
-                            {flows.map(f => (
-                                <Flow key={f.id} flow={f} opts={opts} />
+                            {drops.map(d => (
+                                <Drop drop={d} opts={opts} />
                             ))}
                         </g>
                     </svg>
